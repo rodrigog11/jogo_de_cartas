30693a0856d4923490e2dc226f6668d6eb0c9fd1
Mia Machado <mariaima@al.insper.edu.br>
2021-05-09 13:28:42 -0300

    Diretrizes em CapsLock quanto ao que falta + final

--- jogo.py
+++ jogo.py
@@ -1,15 +1,17 @@
-#Importando funções e arquivos a serem utilizados
+#Importando funções e arquivos a serem utilizados:
 import time
 from funcoes import *
 
-#Texto inicial - boas-vindas e instruções
-#(uso da biblioteca time para sugerir maior interatividade)
+#Texto inicial - boas-vindas e instruções:
+#(uso da função 'time.sleep' para sugerir maior interatividade)
 time.sleep(0)
 print("\033[1m" + "|Paciência Acordeão|\n" + "====================" +"\033[0m" + "\n" )
 time.sleep(0)
-print("Seja bem-vindo(a) ao jogo de Paciência Acordeão! O objetivo deste jogo é colocar todas as cartas em uma mesma pilha. \n")
+
+#COLOCAR A POSSIBILIDADE DE DAR SKIP DAS INSTRUÇÕES
+print("Seja bem-vindo(a) ao jogo de Paciência Acordeão! O objetivo deste jogo é colocar todas as cartas em uma mesma pilha.\n")
 time.sleep(0)
-print("Existem apenas dois movimentos possíveis: \n" )
+print("Existem apenas dois movimentos possíveis:\n" )
 time.sleep(0)
 print ("1. Empilhar uma carta sobre a carta imediatamente anterior")
 time.sleep(0)
@@ -23,7 +25,7 @@ print("2. As duas cartas possuem o mesmo naipe.\n")
 time.sleep(0)
 print("Desde que alguma das condições acima seja satisfeita, qualquer carta pode ser movimentada.\n")
 time.sleep(0)
-#Input inicial (começa o jogo)
+#Input inicial (começa o jogo):
 input("Aperte [ENTER] para para iniciar o jogo...")
 
 #Igualando a lista retornada pela função 'cria_baralho' (um baralho) à lista 'baralho':
@@ -36,7 +38,12 @@ while possui_movimentos_possiveis(baralho):
     
     #Armazenando a carta a ser movida na variável i (numerador da carta; não representa o índice):
     i = int(input('Escolha uma carta (digite um número entre 1 e {0}): '.format(len(baralho))))
-    
+
+    #No caso de i não corresponder a uma carta existente, perguntando novamente qual carta o/a jogador(a) deseja mover:
+    #PRECISA CONECTAR ESSE WHILE/FOR COM O ELSE (SE NÃO FOR POSSÍVEL MOVER A CARTA)
+    while:
+        i= int(input("Posição inválida. Por favor, digite um número entre 1 e {0}: ".format(len(baralho)))) 
+        
     #Armazenando a lista de movimentos possíveis para a carta escolhida (i) na lista 'mov':
     mov = lista_movimentos_possiveis(baralho, i-1)
 
@@ -51,6 +58,7 @@ while possui_movimentos_possiveis(baralho):
 
     #Se a lista mov tiver dois itens (ou mais):
     elif len(mov) > 1:
+        #TEM DE EXIBIR AS CARTAS DAS POSIÇÕES DE DESTINO, EXEMPLO: 1.(CARTA 1) \n 2.(CARTA 2) )
         onde_empilha = int(input('Sobre qual carta você quer empilhar {0}? '.format(baralho[i-1])))
         if onde_empilha == 1:
             empilhar = empilha(baralho,i-1 ,i-2)
@@ -59,8 +67,21 @@ while possui_movimentos_possiveis(baralho):
             empilhar=empilha(baralho,i-1,i-4)
             baralho = empilhar
 
+            #ESTE ELSE PRECISA SER CONECTADO COM O WHILE (DO INPUT DO I Ñ SER VÁLIDO) E COM A LISTA 'MOV'
     else:
         #Imprimindo mensagem referente à impossibilidade de mover a carta escolhida:
-        i = int(input("A carta {0} não pode ser movida. Por favor, digite um número entre 1 e {1}: ".format(baralho[i-1], len(baralho)))) 
+        i = int(input("A carta {0} não pode ser movida. Por favor, digite um número entre 1 e {1}: ".format(baralho[i-1], len(baralho))))
 
-#
+#Printando o estado final do baralho, quando houver nenhum movimento possível
+print((baralho) + "Não há mais movimentos possíveis.")
+
+#Resultado do jogo:
+if len(baralho) > 10:
+    print("Você perdeu. {0} cartas foram eliminadas.".format(len(baralho)))
+else:
+    if len(baralho) > 5:
+        print("Você chegou perto! {0} cartas foram eliminadas.".format(len(baralho)))
+    elif len(baralho) > 1:
+        print("Você quase ganhou! {0} cartas foram eliminadas. Muito bem!".format(len(baralho)))
+    elif len(baralho) == 1:
+        print("Você ganhou - "+ "\033[1m" + "parabéns!" + "\033[0m")
